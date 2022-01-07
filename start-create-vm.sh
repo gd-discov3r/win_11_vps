a3439706241b629c8b02f590b709c4886ac4b76c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-07 11:31:44 +0700

    Update start-create-vm.sh

--- start-create-vm.sh
+++ start-create-vm.sh
@@ -4,9 +4,9 @@ stty susp undef
 
 if [ -d "clouddrive" ]
 then
-    echo "Not In Cloud Shell, Please Run It In Azure SandBox" && exit
-else
     echo "Script by fb.com/thuong.hai.581"
+else
+    echo "Not In Cloud Shell, Please Run It In Azure SandBox" && exit
 fi
 
 curl -s -L -k -O https://github.com/kmille36/Windows-11-VPS/raw/main/az-cli-vm-hk.sh
